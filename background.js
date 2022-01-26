5905cee1c9ad8f33a8096f01af3e55a466ef9fd6
kaspar <kasper@crewnew.com>
2022-01-26 15:55:20 +0000

    Remove not needed files

--- background.js
+++
@@ -1,5 +0,0 @@
-// Get the URL of current open tab: https://stackoverflow.com/questions/1979583/how-can-i-get-the-url-of-the-current-tab-from-a-google-chrome-extension
-chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-    let url = tabs[0].url;
-    // use `url` here inside the callback because it's asynchronous!
-});
